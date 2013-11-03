d0a351831df5075a6eb44f89d8ae3af4539d319c
tyr <tyr.asd@gmail.com>
2013-11-03 20:51:48 +0100

    faster xml parsing
    using a streaming xml parser (htmlparser2) instead of xmldom speeds
    up things significantly. especially with very large xml files.

---
+++ parse_osmxml.js
@@ -0,0 +1,37 @@
+/* converts OSM XML to OSM JSON using a fast streaming parser */
+var htmlparser = require('htmlparser2');
+var _ = require('lodash');
+
+var json = {
+    "version": 0.6,
+    "elements": []
+};
+var buffer = {};
+var p = new htmlparser.Parser({
+    onopentag: function(name, attr) {
+        if (name === "node" || name === "way" || name === "relation" || name === "area") {
+            buffer = {
+                type: name
+            }
+            _.merge(buffer, attr);
+        } else if (name === "tag") {
+            if (!buffer.tags) buffer.tags = {};
+            buffer.tags[attr.k] = attr.v;
+        } else if (name === "nd") {
+            if (!buffer.nodes) buffer.nodes = [];
+            buffer.nodes.push(attr.ref);
+        } else if (name === "member") {
+            if (!buffer.members) buffer.members = [];
+            buffer.members.push(attr);
+        }
+    },
+    ontext: function(text) {
+    },
+    onclosetag: function(name) {
+        if (name === "node" || name === "way" || name === "relation" || name === "area") {
+            json.elements.push(buffer);
+        }
+    }
+});
+
+module.exports = function(xml_str) { p.write(xml_str); p.end(); return json; }
