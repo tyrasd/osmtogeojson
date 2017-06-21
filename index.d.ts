adf226cdd1f2b07475968070561fee12c7bb595f
Tim Schumacher <tim.schumacher@flyacts.com>
2017-06-21 16:01:07 +0200

    Add types for usage in typescript

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import { FeatureCollection, Feature, GeometryObject } from 'geojson';
+
+export = osmtogeojson;
+export as namespace osmtogeojson;
+
+declare var osmtogeojson: osmtogeojson.OsmToGeoJsonStatic;
+
+declare namespace osmtogeojson {
+    interface OsmToGeoJSONOptions {
+        verbose: boolean;
+        flatProperties: boolean;
+        // tslint:disable-next-line:no-any
+        uninterestingTags: any;
+        // tslint:disable-next-line:no-any
+        polygonFeatures: any;
+        // tslint:disable-next-line:no-any
+        deduplicator: any;
+    }
+
+    export interface OsmToGeoJsonStatic {
+        // tslint:disable-next-line:no-any
+        (data: any, options?: OsmToGeoJSONOptions): FeatureCollection<GeometryObject>;
+    }
+}
