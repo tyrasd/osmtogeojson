6a24075a8c81ea1bb51c41feebdb2a3e3e0760b2
Roland Olbricht <roland.olbricht@gmx.de>
2018-01-24 20:15:46 +0100

    First implementation of derived geometry format.

--- parse_osmxml.js
+++ parse_osmxml.js
@@ -7,6 +7,8 @@ var json = {
     "elements": []
 };
 var buffer = {};
+var tagStack = new Array();
+var geomStack = [ {} ];
 var p = new htmlparser.Parser({
     onopentag: function(name, attr) {
         switch (name) {
@@ -58,18 +60,99 @@ var p = new htmlparser.Parser({
                 maxlat: attr.maxlat,
                 maxlon: attr.maxlon
             };
+        break;
+        case "point":
+            if (!Array.isArray(geomStack) || geomStack.length == 0)
+                geomStack = [ {} ];
+            geomStack[geomStack.length-1] = {
+                "type":"Point",
+                "coordinates":[ Number(attr.lon), Number(attr.lat) ]
+            };
+        break;
+        case "vertex":
+            if (!Array.isArray(geomStack) || geomStack.length == 0)
+                geomStack = [ {} ];
+            if (geomStack[geomStack.length-1].type !== "LineString" ||
+                    !Array.isArray(geomStack[geomStack.length-1].coordinates))
+                geomStack[geomStack.length-1] = {
+                    "type":"LineString",
+                    "coordinates":[]
+                };
+            geomStack[geomStack.length-1].coordinates.push([ Number(attr.lon), Number(attr.lat) ]);
+        break;
+        case "linestring":
+            if (!Array.isArray(geomStack) || geomStack.length == 0)
+                geomStack = [ {} ];
+            if (geomStack[geomStack.length-1].type !== "Polygon" ||
+                    !Array.isArray(geomStack[geomStack.length-1].coordinates))
+                geomStack[geomStack.length-1] = {
+                    "type":"Polygon",
+                    "coordinates":[]
+                };
+            geomStack.push({});
+        break;
+        case "group":
+            if (!Array.isArray(geomStack) || geomStack.length == 0)
+                geomStack = [ {} ];
+            if (geomStack[geomStack.length-1].type !== "GeometryCollection" ||
+                    !Array.isArray(geomStack[geomStack.length-1].geometries))
+                geomStack[geomStack.length-1] = {
+                    "type":"GeometryCollection",
+                    "geometries":[]
+                };
+            geomStack.push({});
+        break;
+        default:
+            if (Array.isArray(tagStack) && !tagStack.length == 0 && tagStack[tagStack.length-1] === "osm")
+            {
+                buffer = {
+                    type: name,
+                    tags: {}
+                }
+                _.merge(buffer, attr);
+            }
         }
+        if (!tagStack || !Array.isArray(tagStack))
+            tagStack = new Array();
+        tagStack.push(name);
     },
     ontext: function(text) {
     },
     onclosetag: function(name) {
-        if (name === "node" || name === "way" || name === "relation" || name === "area") {
+        if (Array.isArray(tagStack))
+            tagStack.pop();
+        if (Array.isArray(tagStack) && !tagStack.length == 0 && tagStack[tagStack.length-1] === "osm") {
             // remove empty geometry or nodes arrays
             if (buffer.geometry && buffer.geometry.every(function(g) {return g===null;}))
                 delete buffer.geometry;
             if (name === "relation")
                 delete buffer.nodes;
+            buffer.type = name;
+            if (Array.isArray(geomStack) && geomStack.length > 0 && geomStack[0].type)
+                buffer.geometry = geomStack[0];
             json.elements.push(buffer);
+            buffer = {};
+            geomStack = [ {} ];
+        }
+        else if (name === "linestring")
+        {
+            if (Array.isArray(geomStack) && geomStack.length > 1
+                && Array.isArray(geomStack[geomStack.length - 2].coordinates)
+                && Array.isArray(geomStack[geomStack.length - 1].coordinates)
+                && geomStack[geomStack.length - 1].type === "LineString")
+            {
+                geomStack[geomStack.length - 2].coordinates.push(geomStack[geomStack.length - 1].coordinates);
+                geomStack.pop();
+            }
+        }
+        else if (name === "group")
+        {
+            if (Array.isArray(geomStack) && geomStack.length > 1
+                && Array.isArray(geomStack[geomStack.length - 2].geometries))
+            {
+                geomStack[geomStack.length - 2].geometries.push(geomStack[geomStack.length - 1]);
+                geomStack.pop();
+            }
         }
         if (name === "member") {
             if (buffer.geometry) {
